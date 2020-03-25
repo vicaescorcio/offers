75f7366b1e5c7f1815be557fa8a12c8eabccd4a7
Vicente Lucas <vica.escorcio@gmail.com>
2020-03-25 19:10:10 -0300

    Sets active_admin

---
+++ app/admin/offers.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Offer do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :advertiser_name, :url, :description, :starts_end, :ends_at, :premium
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:advertiser_name, :url, :description, :starts_end, :ends_at, :premium]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
